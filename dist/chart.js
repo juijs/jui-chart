e6ba583dfabfd8818a80dcd89cdc7e7681c97267
seogi1004 <seogi777@gmail.com>
2016-02-01 11:28:35 +0900

    그리드 라인이 잘못나오는 버그, 코드 롤백...

--- dist/chart.js
+++ dist/chart.js
@@ -4457,16 +4457,16 @@ jui.define("chart.grid.draw2d", [ "util.base", "util.math" ], function(_, math) 
                 isDrawLine = false;
 
             if (position == "top") {
-                isDrawLine = this.checkDrawLineX(index, isLast);
+                isDrawLine = this.checkDrawLineY(index, isLast);
                 area = { x1: 0, x2: 0, y1: 0, y2: this.axis.area("height") };
             } else if (position == "bottom" ) {
-                isDrawLine = this.checkDrawLineX(index, isLast);
+                isDrawLine = this.checkDrawLineY(index, isLast);
                 area = { x1: 0, x2: 0, y1: 0, y2: -this.axis.area("height") };
             } else if (position == "left") {
-                isDrawLine = this.checkDrawLineY(index, isLast);
+                isDrawLine = this.checkDrawLineX(index, isLast);
                 area = { x1: 0, x2: this.axis.area("width"), y1: 0, y2: 0 };
             } else if (position == "right" ) {
-                isDrawLine = this.checkDrawLineY(index, isLast);
+                isDrawLine = this.checkDrawLineX(index, isLast);
                 area = { x1: 0, x2: -this.axis.area("width"), y1: 0, y2: 0 };
             }
 
