a71e96c61345c08bd0a272b633682bad4182ce10
seogi1004 <seogi777@gmail.com>
2016-02-21 02:47:15 +0900

    Path brush, related axis bug fixed

--- js/brush/path.js
+++ js/brush/path.js
@@ -4,27 +4,27 @@ jui.define("chart.brush.path", [], function() {
      * @class chart.brush.path 
      * @extends chart.brush.core
      */
-	var PathBrush = function(chart, axis, brush) {
+	var PathBrush = function() {
 
 		this.draw = function() {
-			var g = this.chart.svg.group();
+			var g = this.svg.group();
 			
 			for(var ti = 0, len = this.brush.target.length; ti < len; ti++) {
 				var color = this.color(ti);
 
-				var path = chart.svg.path({
+				var path = this.svg.path({
 					fill : color,
-					"fill-opacity" : chart.theme("pathBackgroundOpacity"),
+					"fill-opacity" : this.chart.theme("pathBackgroundOpacity"),
 					stroke : color,
-					"stroke-width" : chart.theme("pathBorderWidth")
+					"stroke-width" : this.chart.theme("pathBorderWidth")
 				});
 	
 				g.append(path);
 	
 				this.eachData(function(i, data) {
-					var obj = this.axis.c(i, data[brush.target[ti]]),
-						x = obj.x - chart.area("x"),
-						y = obj.y - chart.area("y");
+					var obj = this.axis.c(i, data[this.brush.target[ti]]),
+						x = obj.x - this.chart.area("x") + this.axis.padding("left"),
+						y = obj.y - this.chart.area("y") + this.axis.padding("top");
 
 					if (i == 0) {
 						path.MoveTo(x, y);
