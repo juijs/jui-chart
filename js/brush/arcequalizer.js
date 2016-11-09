ec7ab02a22d73739eca7431eb9cb46fd48939aeb
seogi1004 <seogi777@gmail.com>
2016-11-09 17:56:02 +0900

    Add the logic needed for the operation

--- js/brush/arcequalizer.js
+++ js/brush/arcequalizer.js
@@ -4,21 +4,71 @@ jui.define("chart.brush.arcequalizer", [], function() {
      * @class chart.brush.arcequalizer
      */
     var ArcEqualizerBrush = function() {
-        var g;
+        var g, r = 0, cx = 0, cy = 0;
+
+        function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+            var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+            return {
+                x: centerX + (radius * Math.cos(angleInRadians)),
+                y: centerY + (radius * Math.sin(angleInRadians))
+            };
+        }
+
+        function describeArc(x, y, radius, startAngle, endAngle){
+
+            var endAngleOriginal = endAngle;
+            if(endAngleOriginal - startAngle === 360){
+                endAngle = 359;
+            }
+
+            var start = polarToCartesian(x, y, radius, endAngle);
+            var end = polarToCartesian(x, y, radius, startAngle);
+
+            var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+            if(endAngleOriginal - startAngle === 360){
+                var d = [
+                    "M", start.x, start.y,
+                    "A", radius, radius, 0, arcSweep, 0, end.x, end.y, "z"
+                ].join(" ");
+            }
+            else{
+                var d = [
+                    "M", start.x, start.y,
+                    "A", radius, radius, 0, arcSweep, 0, end.x, end.y
+                ].join(" ");
+            }
+
+            return d;
+        }
 
         this.drawBefore = function() {
             g = this.svg.group();
+
+            var area = this.axis.c(0),
+                dist = Math.abs(area.width - area.height);
+
+            r = Math.min(area.width, area.height) / 2;
+            cx = r + ((area.width > area.height) ? dist / 2 : 0);
+            cy = r + ((area.width < area.height) ? dist / 2 : 0);
         }
 
         this.draw = function() {
+            g.append(this.svg.path({
+                d: describeArc(cx, cy, r, 0, 90),
+                fill: "transparent",
+                stroke: "red",
+                "stroke-width": 10
+            }));
+
             return g;
         }
     }
 
     ArcEqualizerBrush.setup = function() {
         return {
-            /** @cfg {Number} [unit=5] Determines the reference value that represents the color.*/
-            unit: 1
+            clip: false
         };
     }
 
