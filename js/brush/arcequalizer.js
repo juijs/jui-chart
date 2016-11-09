5a9e68defe784fa4fece2722c89ce9adf80cd0c6
seogi1004 <seogi777@gmail.com>
2016-11-09 21:54:16 +0900

    Add the logic needed for the operation

--- js/brush/arcequalizer.js
+++ js/brush/arcequalizer.js
@@ -5,6 +5,7 @@ jui.define("chart.brush.arcequalizer", [], function() {
      */
     var ArcEqualizerBrush = function() {
         var g, r = 0, cx = 0, cy = 0;
+        var innerRadius = 50, stackSize = 0;
 
         function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
@@ -15,32 +16,24 @@ jui.define("chart.brush.arcequalizer", [], function() {
             };
         }
 
-        function describeArc(x, y, radius, startAngle, endAngle){
-
+        function describeArc(p, x, y, radius, startAngle, endAngle){
             var endAngleOriginal = endAngle;
             if(endAngleOriginal - startAngle === 360){
                 endAngle = 359;
             }
 
-            var start = polarToCartesian(x, y, radius, endAngle);
-            var end = polarToCartesian(x, y, radius, startAngle);
+            var start = polarToCartesian(x, y, radius, endAngle),
+                end = polarToCartesian(x, y, radius, startAngle),
+                arcSweep = endAngle - startAngle <= 180 ? false : true;
 
-            var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
-
-            if(endAngleOriginal - startAngle === 360){
-                var d = [
-                    "M", start.x, start.y,
-                    "A", radius, radius, 0, arcSweep, 0, end.x, end.y, "z"
-                ].join(" ");
+            if(endAngleOriginal - startAngle === 360) {
+                p.MoveTo(start.x, start.y);
+                p.Arc(radius, radius, 0, arcSweep, 0, end.x, end.y);
+                p.ClosePath();
+            } else {
+                p.MoveTo(start.x, start.y);
+                p.Arc(radius, radius, 0, arcSweep, 0, end.x, end.y);
             }
-            else{
-                var d = [
-                    "M", start.x, start.y,
-                    "A", radius, radius, 0, arcSweep, 0, end.x, end.y
-                ].join(" ");
-            }
-
-            return d;
         }
 
         this.drawBefore = function() {
@@ -52,15 +45,19 @@ jui.define("chart.brush.arcequalizer", [], function() {
             r = Math.min(area.width, area.height) / 2;
             cx = r + ((area.width > area.height) ? dist / 2 : 0);
             cy = r + ((area.width < area.height) ? dist / 2 : 0);
+
+            stackSize = (r - innerRadius) / this.brush.stackCount;
         }
 
         this.draw = function() {
-            g.append(this.svg.path({
-                d: describeArc(cx, cy, r, 0, 90),
+            var p = this.svg.path({
                 fill: "transparent",
                 stroke: "red",
-                "stroke-width": 10
-            }));
+                "stroke-width": 1,
+            });
+
+            describeArc(p, cx, cy, r, 0, 90);
+            g.append(p);
 
             return g;
         }
@@ -68,7 +65,8 @@ jui.define("chart.brush.arcequalizer", [], function() {
 
     ArcEqualizerBrush.setup = function() {
         return {
-            clip: false
+            clip: false,
+            stackCount: 25
         };
     }
 
