074b61d8e92a5eb3b6ee76a97985ff3a631f8a65
seogi1004 <seogi777@gmail.com>
2016-11-10 13:25:09 +0900

    Add the logic needed for the drawing

--- dist/chart.js
+++ dist/chart.js
@@ -16187,7 +16187,7 @@ jui.define("chart.brush.arcequalizer", [], function() {
      */
     var ArcEqualizerBrush = function() {
         var g, r = 0, cx = 0, cy = 0;
-        var innerRadius = 50, stackSize = 0;
+        var innerRadius = 50, stackSize = 0, stackAngle = 0;
 
         function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
@@ -16198,24 +16198,35 @@ jui.define("chart.brush.arcequalizer", [], function() {
             };
         }
 
-        function describeArc(p, x, y, radius, startAngle, endAngle){
+        function describeArc(radius, startAngle, endAngle) {
             var endAngleOriginal = endAngle;
             if(endAngleOriginal - startAngle === 360){
                 endAngle = 359;
             }
 
-            var start = polarToCartesian(x, y, radius, endAngle),
-                end = polarToCartesian(x, y, radius, startAngle),
+            var start = polarToCartesian(cx, cy, radius, endAngle),
+                end = polarToCartesian(cx, cy, radius, startAngle),
                 arcSweep = endAngle - startAngle <= 180 ? false : true;
 
-            if(endAngleOriginal - startAngle === 360) {
-                p.MoveTo(start.x, start.y);
-                p.Arc(radius, radius, 0, arcSweep, 0, end.x, end.y);
-                p.ClosePath();
-            } else {
-                p.MoveTo(start.x, start.y);
-                p.Arc(radius, radius, 0, arcSweep, 0, end.x, end.y);
+            return {
+                sx: end.x,
+                sy: end.y,
+                ex: start.x,
+                ey: start.y,
+                sweep: arcSweep
             }
+        }
+
+        function drawPath(p, radius, startAngle, endAngle) {
+            var arc1 = describeArc(radius, startAngle, endAngle),
+                arc2 = describeArc(radius + stackSize, startAngle, endAngle);
+
+            p.MoveTo(arc1.sx, arc1.sy);
+            p.Arc(radius, radius, 0, arc1.sweep, 1, arc1.ex, arc1.ey);
+            p.LineTo(arc2.ex, arc2.ey);
+            p.Arc(radius + stackSize, radius + stackSize, 0, arc2.sweep, 0, arc2.sx, arc2.sy);
+            p.LineTo(arc1.sx, arc1.sy);
+            p.ClosePath();
         }
 
         this.drawBefore = function() {
@@ -16229,17 +16240,24 @@ jui.define("chart.brush.arcequalizer", [], function() {
             cy = r + ((area.width < area.height) ? dist / 2 : 0);
 
             stackSize = (r - innerRadius) / this.brush.stackCount;
+            stackAngle = 360 / this.listData().length;
         }
 
         this.draw = function() {
-            var p = this.svg.path({
-                fill: "transparent",
-                stroke: "red",
-                "stroke-width": 1,
-            });
+            this.eachData(function(data, index) {
+                var p = this.svg.path({
+                    fill: this.color(index),
+                    stroke: "white",
+                    "stroke-width": 0.5
+                });
 
-            describeArc(p, cx, cy, r, 0, 90);
-            g.append(p);
+                var count = Math.floor((Math.random() * this.brush.stackCount) + 1);
+                for(var i = 0; i < count; i++) {
+                    drawPath(p, innerRadius + (i * stackSize), index * stackAngle, (index + 1) * stackAngle);
+                }
+
+                g.append(p);
+            });
 
             return g;
         }
