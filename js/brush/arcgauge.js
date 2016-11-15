d798851b25a8f90b9b3d1cabb872e46c8c4b6dd3
seogi1004 <seogi777@gmail.com>
2016-11-15 10:57:05 +0900

    Arc gauge code refactoring...

--- js/brush/arcgauge.js
+++ js/brush/arcgauge.js
@@ -4,12 +4,10 @@ jui.define("chart.brush.arcgauge", [ "util.base" ], function(_) {
      * @class chart.brush.arcgauge
      */
     var ArcGaugeBrush = function() {
-        var self = this;
         var g, r = 0, cx = 0, cy = 0;
-        var stackSize = 0, stackAngle = 0, dataCount = 0;
 
-        function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-            var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+        this.polarToCartesian = function(centerX, centerY, radius, angleInDegrees) {
+            var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
             return {
                 x: centerX + (radius * Math.cos(angleInRadians)),
@@ -17,15 +15,15 @@ jui.define("chart.brush.arcgauge", [ "util.base" ], function(_) {
             };
         }
 
-        function describeArc(radius, startAngle, endAngle) {
+        this.describeArc = function(centerX, centerY, radius, startAngle, endAngle) {
             var endAngleOriginal = endAngle;
 
             if(endAngleOriginal - startAngle === 360){
                 endAngle = 359.9;
             }
 
-            var start = polarToCartesian(cx, cy, radius, endAngle),
-                end = polarToCartesian(cx, cy, radius, startAngle),
+            var start = this.polarToCartesian(centerX, centerY, radius, endAngle),
+                end = this.polarToCartesian(centerX, centerY, radius, startAngle),
                 arcSweep = endAngle - startAngle <= 180 ? false : true;
 
             return {
@@ -37,73 +35,33 @@ jui.define("chart.brush.arcgauge", [ "util.base" ], function(_) {
             }
         }
 
-        function drawPath(p, radius, startAngle, endAngle) {
-            var arc1 = describeArc(radius, startAngle, endAngle),
-                arc2 = describeArc(radius + stackSize, startAngle, endAngle);
+        this.calculateArea = function() {
+            var area = this.axis.c(0),
+                dist = Math.abs(area.width - area.height),
+                r = Math.min(area.width, area.height) / 2;
+
+            return {
+                radius: r,
+                width: r - this.brush.textRadius,
+                centerX: r + ((area.width > area.height) ? dist / 2 : 0),
+                centerY: r + ((area.width < area.height) ? dist / 2 : 0)
+            }
+        }
+
+        this.drawStroke = function(p, radius, width, startAngle, endAngle) {
+            var arc1 = this.describeArc(radius, startAngle, endAngle),
+                arc2 = this.describeArc(radius + width, startAngle, endAngle);
 
             p.MoveTo(arc1.sx, arc1.sy);
             p.Arc(radius, radius, 0, arc1.sweep, 1, arc1.ex, arc1.ey);
             p.LineTo(arc2.ex, arc2.ey);
-            p.Arc(radius + stackSize, radius + stackSize, 0, arc2.sweep, 0, arc2.sx, arc2.sy);
+            p.Arc(radius + width, radius + width, 0, arc2.sweep, 0, arc2.sx, arc2.sy);
             p.LineTo(arc1.sx, arc1.sy);
             p.ClosePath();
         }
 
-        function calculateData() {
-            var total = 0,
-                targets = self.brush.target,
-                maxValue = self.brush.maxValue,
-                stackData = [];
-
-            // 스택의 최대 개수를 콜백으로 설정
-            if(_.typeCheck("function", maxValue)) {
-                maxValue = 0;
-
-                self.eachData(function(data) {
-                    maxValue = Math.max(maxValue, self.brush.maxValue.call(self, data));
-                });
-            }
-
-            self.eachData(function(data, i) {
-                stackData[i] = [];
-
-                for(var j = 0; j < targets.length; j++) {
-                    var key = targets[j],
-                        rate = data[key] / maxValue;
-
-                    total += data[key];
-                    stackData[i][j] = Math.ceil(self.brush.stackCount * rate);
-                }
-            });
-
-            // 값이 없을 경우
-            if(stackData.length == 0) {
-                stackData[0] = [];
-
-                for(var j = 0; j < targets.length; j++) {
-                    stackData[0][j] = (j == 0) ? self.brush.stackCount : 0;
-                }
-            }
-
-            return {
-                total: total,
-                data: stackData
-            };
-        }
-
         this.drawBefore = function() {
             g = this.svg.group();
-
-            var area = this.axis.c(0),
-                dist = Math.abs(area.width - area.height);
-
-            r = Math.min(area.width, area.height) / 2;
-            cx = r + ((area.width > area.height) ? dist / 2 : 0);
-            cy = r + ((area.width < area.height) ? dist / 2 : 0);
-
-            dataCount = this.listData().length;
-            stackSize = (r - this.brush.textRadius) / this.brush.stackCount;
-            stackAngle = 360 / (dataCount == 0 ? 1 : dataCount);
         }
 
         this.draw = function() {
@@ -155,7 +113,6 @@ jui.define("chart.brush.arcgauge", [ "util.base" ], function(_) {
         return {
             clip: false,
             maxValue: 100,
-            stackCount: 25,
             textRadius: 50,
             format: null
         };
