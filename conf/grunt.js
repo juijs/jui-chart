9d4285e2ed378a403eb5dcb2febddd94afbd0a49
seogi1004 <seogi777@gmail.com>
2018-06-05 21:51:25 +0900

    2.1.0-rc.10 version released.  - Webpack bundler supported

--- conf/grunt.js
+++ conf/grunt.js
@@ -1,7 +1,7 @@
 // Node.js 및 webpack 환경에서 jui 코어와 연동해서 사용하기 위한 코드
 if (typeof module == 'object' && module.exports) {
     try {
-        module.exports = require("juijs-core");
+        module.exports = require("juijs");
     } catch(e) {
         console.log("JUI_WARNING_MSG: Base module does not exist");
     }
