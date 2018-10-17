e020ba23418c2bb8dc683392dd3ac9163b0849da
seogi1004 <seogi777@gmail.com>
2018-10-17 17:54:28 +0900

    dot3d brush added.

--- bundles/index.js
+++ bundles/index.js
@@ -1,153 +1,35 @@
 import jui from '../src/main.js'
-import TopologyNodeBrush from '../src/brush/topologynode.js'
-import TopologyCtrlWidget from '../src/widget/topologyctrl.js'
-import ClassicIcon from '../src/icon/classic.js'
+import CanvasDot3DBrush from '../src/brush/canvas/dot3d.js'
 
-jui.use([ TopologyNodeBrush, TopologyCtrlWidget, ClassicIcon ]);
-
-function rnd(count) {
-    return Math.floor(Math.random() * count);
+function f1(x) {
+    const a = 10;
+    return (a * Math.pow((x - 4), 2)) + 7;
 }
 
-function getRandomData(count, outer, inner) { // 노드 개수, 바깥 연결 개수, 안쪽 연결 개수
-    var w = 800, h = 600;
-    var data = [];
-    for(var i = 0; i < count; i++) {
-        var x = rnd(w),
-            y = rnd(h);
-        data.push({ name: "W" + (i + 1), x: x, y: y, outgoing: [], incoming: [] });
-    }
-    for(var i = 0; i < outer; i++) {
-        var index = rnd(count),
-            outerIndex = rnd(count);
-        data[index].outgoing.push(outerIndex);
-    }
-    for(var i = 0; i < inner; i++) {
-        var index = rnd(count),
-            innerIndex = rnd(count);
-        data[index].incoming.push(innerIndex);
-    }
-    return data;
-}
+jui.use([ CanvasDot3DBrush ]);
 
-var data = [
-    { key: "1000_1", name: "W1", type: "was", outgoing: [ "1000_1" ] },
-    { key: "1000_2", name: "W2", type: "was", outgoing: [ "1000_3", "1000_4" ] },
-    { key: "1000_3", name: "W3", type: "was", outgoing: [ "1_2_3_4", "1000_2" ] },
-    { key: "1000_4", name: "W4", type: "server", outgoing: [ "1_2_3_4" ] },
-    { key: "1_2_3_4", name: "Oracle", type: "db", outgoing: [] }
-];
-
-var data2 = [
-    { key: "1000_1", name: "W1", type: 1, x: 100, y: 100, outgoing: [] },
-    { key: "1000_2", name: "W2", type: 1, x: 100, y: 200, outgoing: [ "1000_1" ] }
-];
-
-var edgeData = [
-    { key: "1000_1:1000_2", count: 3, time: 1000 },
-    { key: "1000_2:1000_3", count: 3, time: 1000 },
-    { key: "1000_2:1000_4", count: 3, time: 1000 },
-    { key: "1000_3:1_2_3_4", count: 3, time: 2000 },
-    { key: "1000_3:1000_2", count: 3, time: 2000 },
-    { key: "1000_4:1_2_3_4", count: 3, time: 2000 }
-];
-
-jui.ready([ "chart.builder", "util.base" ], function(builder, _) {
-    var c = builder("#chart", {
-        icon: {
-            type: "classic",
-            path: [
-                "../images/icon/icomoon.eot",
-                "../images/icon/icomoon.svg",
-                "../images/icon/icomoon.ttf",
-                "../images/icon/icomoon.woff"
-            ]
-        },
-        theme: "classic",
-        padding: 5,
-        axis: [{
-            c: {
-                type: "topologytable"
-            },
-            data: data
-        }],
-        brush: {
-            type: "topologynode",
-            colors: [ "#729ff1" ],
-            edgeData: edgeData,
-            edgeText: function(data, align) {
-                var text = data.time + "/" + data.count;
-                if(align == "end") {
-                    text = text + " →";
-                } else {
-                    text = "← " + text;
-                }
-                return text;
-            },
-            edgeOpacity: function(data) {
-                if(data.time > 1000) return 1;
-                return 0.3;
-            },
-            tooltipTitle: function(data, align) {
-                if(align == "start") {
-                    return data.reverse().join(" ← ");
-                }
-                return data.join(" → ");
-            },
-            tooltipText: function(data) {
-                return "총 응답시간/개수 : " + data.time + "/" + data.count;
-            },
-            /*/
-            nodeImage: function(data) {
-                return "resource/" + data.type + ".png";
-            },
-            /**/
-            nodeText: function(data) {
-                if(data.type == "server") {
-                    return "{server}";
-                } else if(data.type == "was") {
-                    return "{was}";
-                } else {
-                    return "{db}";
-                }
-            },
-            nodeTitle: function(data) {
-                return data.name;
-            },
-            nodeScale: function(data) {
-                var len = data.outgoing.length;
-                if(len > 1) {
-                    return 2;
-                }
-                return 1;
-            },
-            activeEdge: "1000_2:1000_3",
-            //activeNode: "1000_2",
-            activeEvent: "dblclick"
-        },
-        widget: {
-            type: "topologyctrl",
-            zoom: true,
-            move: true
-        },
-        event: {
-            "topology.edgeclick": function(data, e) {
-                console.log("edgeclick ---------------------");
-                console.log(data);
-            },
-            "topology.nodeclick": function(data, e) {
-                console.log("nodeclick ---------------------");
-                console.log(data);
-            },
-            "click": function(obj, e) {
-                console.log("click ----------------");
-                console.log(obj);
-            }
-        },
-        style: {
-            axisBackgroundColor: "#fff",
-            axisBackgroundOpacity: 1,
-            topologyEdgeWidth : 1
-        }
+jui.ready([ "chart.plane" ], function(plane) {
+    var c = plane("#chart", {
+        dimension: "2d",
+        width: 600,
+        height: 600,
+        x: [ -100, 100 ],
+        y: [ -100, 10000 ],
+        padding: 50,
+        step: 10,
+        symbol: "dot",
+        r: 2
     });
+
+    for(let i = -20; i < 20; i++) {
+        c.push([ i, f1(i) ]);
+    }
+    c.commit();
+
+    c.push([ 4, 7 ]);
+    c.commit();
+
+    c.render();
+
+    console.log(Math.sin((190*Math.PI)/180))
 });
---
+++ src/brush/canvas/dot3d.js
@@ -0,0 +1,167 @@
+import jui from '../../main.js';
+
+jui.define("chart.polygon.face", [], function() {
+    var FacePolygon = function(x1, y1, d1, x2, y2, d2, oy) {
+        this.vertices = [
+            new Float32Array([ x1, y1, d1, 1 ]),
+            new Float32Array([ x2, y2, d2, 1 ]),
+            new Float32Array([ x2, oy, d2, 1 ]),
+            new Float32Array([ x1, oy, d2, 1 ])
+        ];
+
+        this.vectors = [];
+    }
+
+    return FacePolygon;
+}, "chart.polygon.core");
+
+export default {
+    name: "chart.brush.canvas.dot3d",
+    extend: "chart.brush.canvas.core",
+    component: function() {
+        const _ = jui.include("util.base");
+        const MathUtil = jui.include("util.math");
+        const PointPolygon = jui.include("chart.polygon.point");
+        const LinePolygon = jui.include("chart.polygon.line");
+        const FacePolygon = jui.include("chart.polygon.face");
+
+        var CanvasDot3DBrush = function () {
+            var firstCacheData = null;
+
+            this.createDot = function(color, r, data) {
+                var x = this.axis.x(data[0]),
+                    y = this.axis.y(data[1]),
+                    z = this.axis.z(data[2]);
+
+                this.addPolygon(new PointPolygon(x, y, z), function (p) {
+                    var tx = p.vectors[0].x,
+                        ty = p.vectors[0].y,
+                        tr = r * MathUtil.scaleValue(z, 0, this.axis.depth, 1, p.perspective);
+
+                    this.drawDot(color, tx, ty, tr);
+                });
+            }
+
+            this.createLine = function(color, r, data, pdata, isLast) {
+                var x = this.axis.x(data[0]),
+                    y = this.axis.y(data[1]),
+                    z = this.axis.z(data[2]),
+                    px = this.axis.x(pdata == null ? data[0] : pdata[0]),
+                    py = this.axis.y(pdata == null ? data[1] : pdata[1]),
+                    pz = this.axis.z(pdata == null ? data[2] : pdata[2]);
+
+                this.addPolygon(new LinePolygon(px, py, pz, x, y, z), function (p) {
+                    var x1 = p.vectors[0].x,
+                        y1 = p.vectors[0].y,
+                        x2 = p.vectors[1].x,
+                        y2 = p.vectors[1].y;
+
+                    this.drawLine(color, x1, y1, x2, y2, r, isLast);
+                });
+            }
+
+            this.createArea = function(color, r, data, pdata) {
+                var oy = this.axis.y(0),
+                    x = this.axis.x(data[0]),
+                    y = this.axis.y(data[1]),
+                    z = this.axis.z(data[2]),
+                    px = this.axis.x(pdata == null ? data[0] : pdata[0]),
+                    py = this.axis.y(pdata == null ? data[1] : pdata[1]),
+                    pz = this.axis.z(pdata == null ? data[2] : pdata[2]);
+
+                this.addPolygon(new FacePolygon(px, py, pz, x, y, z, oy), function (p) {
+                    var x1 = p.vectors[0].x,
+                        y1 = p.vectors[0].y,
+                        x2 = p.vectors[1].x,
+                        y2 = p.vectors[1].y,
+                        x3 = p.vectors[2].x,
+                        y3 = p.vectors[2].y,
+                        x4 = p.vectors[3].x,
+                        y4 = p.vectors[3].y;
+
+                    this.drawArea(color, x1, y1, x2, y2, x3, y3, x4, y4);
+                });
+            }
+
+            this.drawDot = function(color, tx, ty, tr) {
+                this.canvas.beginPath();
+                this.canvas.arc(tx, ty, tr, 0, 2 * Math.PI, false);
+                this.canvas.fillStyle = color;
+                this.canvas.fill();
+            }
+
+            this.drawLine = function(color, x1, y1, x2, y2, width, isLast) {
+                var isFill = this.brush.symbol == "poly";
+
+                if(isFill && firstCacheData == null) {
+                    firstCacheData = arguments;
+                }
+
+                this.canvas.beginPath();
+                this.canvas.moveTo(x1, y1);
+                this.canvas.lineTo(x2, y2);
+                this.canvas.lineWidth = width;
+                this.canvas.strokeStyle = color;
+                this.canvas.stroke();
+
+                if(isLast) {
+                    if(isFill && firstCacheData != null) {
+                        this.canvas.lineTo(firstCacheData[1], firstCacheData[2]);
+                        this.canvas.fillStyle = firstCacheData[0];
+                        this.canvas.fill();
+                    }
+
+                    this.canvas.closePath();
+                }
+            }
+
+            this.drawArea = function(color, x1, y1, x2, y2, x3, y3, x4, y4) {
+                this.canvas.beginPath();
+                this.canvas.moveTo(x1, y1);
+                this.canvas.lineTo(x2, y2);
+                this.canvas.lineTo(x3, y3);
+                this.canvas.lineTo(x4, y4);
+                this.canvas.lineTo(x1, y1);
+                this.canvas.strokeStyle = color;
+                this.canvas.stroke();
+                this.canvas.fillStyle = color;
+                this.canvas.fill();
+                this.canvas.closePath();
+            }
+
+            this.draw = function() {
+                var symbol = this.brush.symbol,
+                    color = this.color(this.brush.color),
+                    r = this.brush.size / 2,
+                    datas = this.listData();
+
+                for(var i = 0; i < datas.length; i++) {
+                    var data = datas[i];
+
+                    // 2d 데이터일 경우, z값을 추가해준다.
+                    if(data.length == 2) {
+                        data.push(0);
+                    }
+
+                    if(symbol == "line" || symbol == "poly") {
+                        this.createLine(color, r, data, (i == 0) ? null : datas[i - 1], (i == data.length-1));
+                    } else if(symbol == "area") {
+                        this.createArea(color, r, data, (i == 0) ? null : datas[i - 1]);
+                    } else {
+                        this.createDot(color, r, data);
+                    }
+                }
+            }
+        }
+
+        CanvasDot3DBrush.setup = function() {
+            return {
+                size: 4,
+                color: 0,
+                symbol: "dot" // or line, area, poly
+            };
+        }
+
+        return CanvasDot3DBrush;
+    }
+}
