2bff707f56bb80447ceacb62769cf79165b6074c
seogi1004 <seogi777@gmail.com>
2018-11-29 14:42:40 +0900

    Webpack deployment mode setting changed.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const entry = path.resolve(__dirname, 'bundles', 'production.js');
 
 module.exports = {
     mode: 'production',
     entry: {
-        'jui-chart': path.resolve(__dirname, 'bundles', 'production.js')
+        'jui-chart': entry,
+        'jui-chart.min': entry
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -13,7 +15,9 @@ module.exports = {
     },
     optimization: {
         minimizer: [
-            // new UglifyJsPlugin()
+            new UglifyJsPlugin({
+                include: /\.min\.js$/
+            })
         ]
     },
     module: {
